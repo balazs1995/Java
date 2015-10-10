bc23c2376d7b430881429ae8ca63ba7120babaf3
Bak Balázs <bak.balazs95@gmail.com>
2015-10-10 14:27:25 +0200

    cat.java

--- Zoo/Cat.java
+++ Zoo/Cat.java
@@ -1,10 +1,13 @@
 public class Cat extends Animals {
-  public void kind(){
-        System.out.println("\nÉn egy macska vagyok. \n Add meg a nevem: ");
+  public Cat(){}
+  public void kind() {
+    setName("Cica");
+    System.out.println("Én egy macska vagyok.");
 
-        System.out.println(" Add meg a súlyom: ");
+    System.out.println("A macska neve: "+getName());
+
+    setWeight(50);
+    System.out.println("A macska súlya: "+getWeight()+" kg");
   }
-
-
 
 }
