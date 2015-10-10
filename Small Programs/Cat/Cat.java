d9f7511cb36bf0ece554ef609438c84d7de2ed5b
Bak Balázs <bak.balazs95@gmail.com>
2015-10-10 23:57:50 +0200

    Cat fejlesztve

--- Small Programs/Cat/Cat.java
+++ Small Programs/Cat/Cat.java
@@ -1,26 +1,30 @@
 import java.util.Scanner;
 import java.util.Random;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
 
 public class Cat{
-  public static void print(String outputfile ) throws IOException {
 
-    System.out.println("\nÉn egy macska vagyok. \n Add meg a nevem: ");
+  private String name ;
+  private int weight;
+
+  public void setWeight(int weight){
+    this.weight = weight;
+  }
+
+  public int getWeight(){
+    return weight;
+  }
+
+  public void setName(String name) {
+        this.name = name;
+	}
+
+  public String getName(){
+    return name;
+  }
+
+  public void data(){
     Scanner in = new Scanner(System.in);
-    String name ;
-    name = in.nextLine();
-    int weight;
-    weight = (int)((Math.random()*901)+100);
-
-    BufferedWriter output = new BufferedWriter(new FileWriter(outputfile));
-
-      output.write("\nDatas:\n");
-      output.write("A macska neve: " + name + "\n");
-      output.write("A " + name  + " súlya : " + weight + "\n");
-
-output.close();
-}
-
-}
+    setName(in.nextLine());
+    setWeight((int)((Math.random()*901)+100));
+  }
+  }
