d3637c40ffaff4c5ed251c63f26b830a720691de
Bak Balázs <bak.balazs95@gmail.com>
2015-11-19 23:33:55 +0100

    AspectjExample maven

---
+++ AspetjExample/src/main/java/ElőtteUtána.aj
@@ -0,0 +1,15 @@
+package hello;
+
+public aspect ElőtteUtána {
+
+	public pointcut fgvHívás(): call(public void HellóVilág.helló());
+
+	before(): fgvHívás() {
+		System.out.println("ElotteUtana> Alma");
+	}
+
+	after(): fgvHívás() {
+		System.out.println("ElotteUtana> Korte");
+	}
+
+}
---
+++ AspetjExample/src/main/java/hello/hello/HellóVilág.java
@@ -0,0 +1,18 @@
+package hello;
+
+public class HellóVilág {
+
+    public void helló() {
+
+        System.out.println("alma");
+        System.out.println("HelloVilag> Hello!");
+        System.out.println("korte");
+
+    }
+
+    public static void main(String[] args) {
+
+        new HellóVilág().helló();
+    }
+
+}
